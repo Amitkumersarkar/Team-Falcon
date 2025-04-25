f24a840426a1fd8619c9a18f9242de8689a8c968
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-04-26 02:34:51 +0600

    updated file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
 import { useState } from 'react';
 import './App.css';
-import Available from './components/AvailableSection/Available'
 import Banner from './components/Banner/Banner'
 import Navbar from './components/Header/Navbar'
 import AvailablePlayer from './components/MainSection/AvailablePlayer'
-import Selected from './components/SelectedSection/Selected'
 import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
+  // state management and props drilling
   const [credit, setCredit] = useState(0);
   const handleAddFreeCredit = () => {
     setCredit(credit + 10);
     console.log("Button clicked! Adding credit...");
     toast('Thank You For Claiming Free Credit..!!', { position: "top-center" });
+
+    // using conditional rendering
+    const [isActive, setIsActive] = useState({
+      available: true,
+      status: "active"
+    })
+    // declaring event handler
+    const handleIsActiveState = (status) => {
+      // condition apply
+      if (status == "available") {
+        setIsActive({
+          available: true,
+          status: "available"
+        })
+      }
+      else {
+        setIsActive({
+          available: false,
+          status: "selected"
+        })
+      }
+    }
   }
   return (
     <>
       <Navbar credit={credit}></Navbar>
       <Banner handleAddFreeCredit={handleAddFreeCredit}></Banner>
-      <div className='w-10/12 mx-auto flex justify-between'>
-        <AvailablePlayer></AvailablePlayer>
-        <div className='flex gap-6'>
-          <Available></Available>
-          <div>
-            <Selected></Selected>
-          </div>
-        </div>
-        <div className='font-semibold text-xl'>
-          <ToastContainer />
-        </div>
+      <AvailablePlayer></AvailablePlayer>
+      <div className='font-semibold text-xl'>
+        <ToastContainer />
       </div>
     </>
   )
--- src/components/MainSection/AvailablePlayer.jsx
+++ src/components/MainSection/AvailablePlayer.jsx
@@ -1,9 +1,16 @@
+import Available from '../AvailableSection/Available';
+import Selected from '../SelectedSection/Selected';
 import './AvailablePlayer.css';
 const AvailablePlayer = () => {
     return (
-        <div>
+        <div className='flex justify-between w-11/12 mx-auto '>
             <h1 className='text-2xl font-bold'> Available Players </h1>
-
+            <div className='flex gap-6'>
+                <Available></Available>
+                <div>
+                    <Selected></Selected>
+                </div>
+            </div>
         </div>
     );
 };
