41d18e5273036351d0013a53a4d1e00328f3086b
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-04-25 21:48:23 +0600

    added all file

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,18 @@
+import './Banner.css';
+const Banner = () => {
+    return (
+        <div className='w-11/12 mx-auto p-7 font-bold'>
+
+            <div className='bgg h1'>
+                <img className='items-center' src="../../../public/assets/banner-main.png" alt="" />
+                <div className='text-5xl font-semibold text-white pt-5'>
+                    <h2>Assemble Your Ultimate Team Falcon Cricket Team</h2>
+                </div>
+                <p className='text-gray-300 text-3xl p-5'>Beyond Boundaries Beyond Limits</p>
+                <button className='text-white font-bold brr'>Claim Free Credit</button>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,24 @@
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-between items-center w-11/12 mx-auto border-b-1 p-5'>
+            <div>
+                <img src="../../../public/assets/logo.png" alt="" />
+            </div>
+            <div className='flex justify-around gap-5 text-xl font-bold '>
+                <a className='hover:text-pink-700' href="">Home</a>
+                <a className='hover:text-pink-700' href="">About</a>
+                <a className='hover:text-pink-700' href="">Teams</a>
+                <a className='hover:text-pink-700' href="">Schedules</a>
+                <div className='flex gap-5'>
+                    <p>$0 M</p>
+                    <img className='w-6 h-6' src="../../../public/assets/coins.png" alt="" />
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Navbar;
